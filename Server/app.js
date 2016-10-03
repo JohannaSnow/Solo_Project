b148222f8dac05489f4cc8458c5d4c27677ffa33
Johanna Snow <johannadsnow@gmail.com>
2016-10-03 15:17:12 -0500

    server is listening

---
+++ Server/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded( { extended: true } );
+var path = require('path');
+var app = express();
+//var mongoose = require('mongoose');
+//var PetModel = require('../models.PetModel'); //not necessary to have this here, need it in routers though!
+var portDecision = process.env.PORT || 8000;
+
+//json body parser
+app.use(bodyParser.json());
+
+//listen and port decision
+app.listen(portDecision, function(){
+  console.log('I can hear you', portDecision);
+});
+
+// // app.get --  serves the index -- get params
+app.get( '/', function( req, res ){
+//res.sendFile( path.resolve( 'public/index.html' ) );
+});
+console.log ('base url hit)');
+
+
+app.use( express.static( 'public' ) );
