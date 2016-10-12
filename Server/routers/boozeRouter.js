1c6d044d49bc9c4d6f8175ed13a8bbe6f8116cdf
Johanna Snow <johannadsnow@gmail.com>
2016-10-12 13:31:08 -0500

    delete route is functional

--- Server/routers/boozeRouter.js
+++ Server/routers/boozeRouter.js
@@ -5,9 +5,10 @@ var mongoose = require('mongoose');
 var urlencodedparser = bodyParser.urlencoded({extended: false});
 // var path = require('path');
 
+console.log('in booze router');
 
 // require model
-var booze = require('../Models/boozeModel');
+var Booze = require('../Models/boozeModel');
 
 // use body-parser
 router.use(urlencodedparser);
@@ -19,17 +20,68 @@ router.get('/:id', function(req, res){
   console.log('in router get');
 
 
-    booze.find({type: req.params.id}, function(err, boozeResults) {
+    Booze.find({type: req.params.id}, function(err, boozeResults) {
     if(err){
       console.log(err);
       res.sendStatus(500);
     }else{
       console.log('this is the booze you were looking for!');
       res.send(boozeResults);
-    }
-  });
-  });
+    } // end else
+  }); // end of find
+}); // end of get
 
 
+  // / // post route
+  router.post('/', function(req, res){
+    console.log('in router post');
+
+    var receivedBooze = req.body;
+    console.log('req.body:', receivedBooze);
+
+  //   // create newBooze
+    var newBooze = new Booze({
+      name: receivedBooze.name,
+      type: receivedBooze.type,
+      description: receivedBooze.description,
+      origin: receivedBooze.origin,
+      ingredients: receivedBooze.ingredients,
+      abv: receivedBooze.abv,
+      safe: receivedBooze.safe,
+      image: receivedBooze.image
+    }); // end create newBooze
+    console.log('Schema:', newBooze);
+
+    // save newBooze to database
+    newBooze.save(function(err){
+      if(err){
+        console.log(err);
+        res.sendStatus(500);
+      } else {
+        console.log('newBooze stored');
+        res.sendStatus(201);
+      } // end else
+    }); // end save newBooze
+  }); // end post route
+
+
+
+// delete route
+router.delete('/', function(req, res){
+  console.log('in delete router');
+
+
+
+  // remove selected booze
+  Booze.remove({_id: req.body._id}, function(err){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log('Booze deleted');
+      res.sendStatus(201);
+    }//end else
+  }); // end remove selected booze
+}); // end delete route
 
 module.exports = router;
